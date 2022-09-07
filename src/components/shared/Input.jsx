dc6f4fe20e137972373d83d1f3cad3a1dda56042
Joseph Enoch <enochjoseph01@gmail.com>
2022-09-07 15:02:07 +0100

    add: install react-feather

--- src/components/shared/Input.jsx
+++ src/components/shared/Input.jsx
@@ -1,16 +1,23 @@
-import React from 'react'
+import React,{useEffect, useState} from 'react'
 
-const Input = ({type, width, addedClasses,...rest }) => {
+const Input = ({propType, width, addedClasses,...rest }) => {
+  const [type, setType] = useState()
+
+  useEffect(()=>{
+    setType(propType)
+  },[propType])
   const addeStyles ={
     width: `w-${width}`,
     others: addedClasses
   }
   return (
-    <input 
-        className={`min-w-[100px] ${addeStyles.width} ${addeStyles.addedClasses} border-[#6D6D6D] border-[1px] rounded-[5px] font-clashGrotesk h-[32px] text-sm px-4 focus:outline-none`} 
-        type={type} 
-        {...rest}
-    />
+    <>
+      <input 
+          className={`min-w-[100px] ${addeStyles.width} ${addeStyles.addedClasses} border-[#6D6D6D] border-[1px] rounded-[5px] font-clashGrotesk h-[32px] text-sm px-4 focus:outline-none`} 
+          type={type} 
+          {...rest}
+      />
+    </>
   )
 }
 
