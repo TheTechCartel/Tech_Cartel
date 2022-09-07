39b71813cdff09d20ea83faf208f564338a4a3e4
Joseph Enoch <enochjoseph01@gmail.com>
2022-09-07 20:30:09 +0100

    comp: complete the input component

--- src/App.js
+++ src/App.js
@@ -4,7 +4,12 @@ import Input from "./components/shared/Input";
 function App() {
   return (
    <main className="w-screen font-clashGrotesk">
-    <Input placeholder="Enter Email Address"/>
+    <Input 
+      addedClasses="" 
+      width="w-[200px]"
+      placeholder="Enter Email Address"
+      type="text"
+    />
    </main>
   );
 }
--- src/components/shared/Input.jsx
+++ src/components/shared/Input.jsx
@@ -1,23 +1,29 @@
-import React,{useEffect, useState} from 'react'
+import React, { useState } from 'react'
+import { Eye } from 'react-feather'
 
-const Input = ({propType, width, addedClasses,...rest }) => {
-  const [type, setType] = useState()
-
-  useEffect(()=>{
-    setType(propType)
-  },[propType])
-  const addeStyles ={
-    width: `w-${width}`,
-    others: addedClasses
+const Input = ({type, width, addedClasses,...rest }) => {
+  const [newType, setNewType] = useState(type)
+  const handleShow = () =>{
+    setNewType(prevState=>{
+      if(prevState.toLowerCase()==="text")return "password"
+      return "text"
+    })
   }
   return (
-    <>
+    <div className={` bg-red-500 ${width} relative h-[32px]`}>
       <input 
-          className={`min-w-[100px] ${addeStyles.width} ${addeStyles.addedClasses} border-[#6D6D6D] border-[1px] rounded-[5px] font-clashGrotesk h-[32px] text-sm px-4 focus:outline-none`} 
-          type={type} 
+          className={`${width} ${addedClasses} relative border-[#6D6D6D] border-[1px] rounded-[5px] font-clashGrotesk  text-sm focus:outline-none h-full text-[#6D6D6D] px-4`} 
+          type={newType} 
           {...rest}
       />
-    </>
+      {type=== "password" &&
+        (<div 
+          className=" flex items-center top-0 absolute -right-[20px] justify-center h-full cursor-pointer text-[#6D6D6D]"
+          onClick={handleShow}
+        >
+          <Eye size={10}/>
+        </div>)}
+    </div>
   )
 }
 
