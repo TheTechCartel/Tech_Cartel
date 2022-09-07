2c4c7c74a5cdd47ba297c3be08469c8e004dd306
Joseph Enoch <enochjoseph01@gmail.com>
2022-09-07 14:58:40 +0100

    feat: add the input component

--- src/components/shared/Input.jsx
+++ src/components/shared/Input.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
 
-const Input = ({type, ...rest}) => {
+const Input = ({type, width, addedClasses,...rest }) => {
+  const addeStyles ={
+    width: `w-${width}`,
+    others: addedClasses
+  }
   return (
-    <input className="min-w-[80%] border-[#6D6D6D] border-[1px] rounded-[5px] font-clashGrotesk h-[32px] text-sm px-4" type={type} {...rest}/>
+    <input 
+        className={`min-w-[100px] ${addeStyles.width} ${addeStyles.addedClasses} border-[#6D6D6D] border-[1px] rounded-[5px] font-clashGrotesk h-[32px] text-sm px-4 focus:outline-none`} 
+        type={type} 
+        {...rest}
+    />
   )
 }
 
--- src/graphql/mutations.js
+++ src/graphql/mutations.js
@@ -1,54 +1,471 @@
 /* eslint-disable */
 // this is an auto generated file. This will be overwritten
 
-export const createTodo = /* GraphQL */ `
-  mutation CreateTodo(
-    $input: CreateTodoInput!
-    $condition: ModelTodoConditionInput
+export const createUser = /* GraphQL */ `
+  mutation CreateUser(
+    $input: CreateUserInput!
+    $condition: ModelUserConditionInput
   ) {
-    createTodo(input: $input, condition: $condition) {
+    createUser(input: $input, condition: $condition) {
       id
-      name
-      description
+      fullname
+      email
+      username
+      bio
+      location
+      isAdmin
+      image
+      posts {
+        items {
+          id
+          content
+          authorID
+          createdAt
+          updatedAt
+          postCategoryId
+          owner
+        }
+        nextToken
+      }
+      nodesCreated {
+        items {
+          id
+          title
+          videoResources
+          webResources
+          bookResources
+          authorID
+          createdAt
+          updatedAt
+          owner
+        }
+        nextToken
+      }
       createdAt
       updatedAt
-      _version
-      _deleted
-      _lastChangedAt
+      owner
     }
   }
 `;
-export const updateTodo = /* GraphQL */ `
-  mutation UpdateTodo(
-    $input: UpdateTodoInput!
-    $condition: ModelTodoConditionInput
+export const updateUser = /* GraphQL */ `
+  mutation UpdateUser(
+    $input: UpdateUserInput!
+    $condition: ModelUserConditionInput
   ) {
-    updateTodo(input: $input, condition: $condition) {
+    updateUser(input: $input, condition: $condition) {
       id
-      name
-      description
+      fullname
+      email
+      username
+      bio
+      location
+      isAdmin
+      image
+      posts {
+        items {
+          id
+          content
+          authorID
+          createdAt
+          updatedAt
+          postCategoryId
+          owner
+        }
+        nextToken
+      }
+      nodesCreated {
+        items {
+          id
+          title
+          videoResources
+          webResources
+          bookResources
+          authorID
+          createdAt
+          updatedAt
+          owner
+        }
+        nextToken
+      }
       createdAt
       updatedAt
-      _version
-      _deleted
-      _lastChangedAt
+      owner
     }
   }
 `;
-export const deleteTodo = /* GraphQL */ `
-  mutation DeleteTodo(
-    $input: DeleteTodoInput!
-    $condition: ModelTodoConditionInput
+export const deleteUser = /* GraphQL */ `
+  mutation DeleteUser(
+    $input: DeleteUserInput!
+    $condition: ModelUserConditionInput
   ) {
-    deleteTodo(input: $input, condition: $condition) {
+    deleteUser(input: $input, condition: $condition) {
       id
-      name
+      fullname
+      email
+      username
+      bio
+      location
+      isAdmin
+      image
+      posts {
+        items {
+          id
+          content
+          authorID
+          createdAt
+          updatedAt
+          postCategoryId
+          owner
+        }
+        nextToken
+      }
+      nodesCreated {
+        items {
+          id
+          title
+          videoResources
+          webResources
+          bookResources
+          authorID
+          createdAt
+          updatedAt
+          owner
+        }
+        nextToken
+      }
+      createdAt
+      updatedAt
+      owner
+    }
+  }
+`;
+export const createPath = /* GraphQL */ `
+  mutation CreatePath(
+    $input: CreatePathInput!
+    $condition: ModelPathConditionInput
+  ) {
+    createPath(input: $input, condition: $condition) {
+      id
+      title
       description
       createdAt
       updatedAt
-      _version
-      _deleted
-      _lastChangedAt
+      owner
+    }
+  }
+`;
+export const updatePath = /* GraphQL */ `
+  mutation UpdatePath(
+    $input: UpdatePathInput!
+    $condition: ModelPathConditionInput
+  ) {
+    updatePath(input: $input, condition: $condition) {
+      id
+      title
+      description
+      createdAt
+      updatedAt
+      owner
+    }
+  }
+`;
+export const deletePath = /* GraphQL */ `
+  mutation DeletePath(
+    $input: DeletePathInput!
+    $condition: ModelPathConditionInput
+  ) {
+    deletePath(input: $input, condition: $condition) {
+      id
+      title
+      description
+      createdAt
+      updatedAt
+      owner
+    }
+  }
+`;
+export const createMiniPath = /* GraphQL */ `
+  mutation CreateMiniPath(
+    $input: CreateMiniPathInput!
+    $condition: ModelMiniPathConditionInput
+  ) {
+    createMiniPath(input: $input, condition: $condition) {
+      id
+      title
+      description
+      createdAt
+      updatedAt
+      owner
+    }
+  }
+`;
+export const updateMiniPath = /* GraphQL */ `
+  mutation UpdateMiniPath(
+    $input: UpdateMiniPathInput!
+    $condition: ModelMiniPathConditionInput
+  ) {
+    updateMiniPath(input: $input, condition: $condition) {
+      id
+      title
+      description
+      createdAt
+      updatedAt
+      owner
+    }
+  }
+`;
+export const deleteMiniPath = /* GraphQL */ `
+  mutation DeleteMiniPath(
+    $input: DeleteMiniPathInput!
+    $condition: ModelMiniPathConditionInput
+  ) {
+    deleteMiniPath(input: $input, condition: $condition) {
+      id
+      title
+      description
+      createdAt
+      updatedAt
+      owner
+    }
+  }
+`;
+export const createNode = /* GraphQL */ `
+  mutation CreateNode(
+    $input: CreateNodeInput!
+    $condition: ModelNodeConditionInput
+  ) {
+    createNode(input: $input, condition: $condition) {
+      id
+      title
+      videoResources
+      webResources
+      bookResources
+      authorID
+      author {
+        id
+        fullname
+        email
+        username
+        bio
+        location
+        isAdmin
+        image
+        posts {
+          nextToken
+        }
+        nodesCreated {
+          nextToken
+        }
+        createdAt
+        updatedAt
+        owner
+      }
+      createdAt
+      updatedAt
+      owner
+    }
+  }
+`;
+export const updateNode = /* GraphQL */ `
+  mutation UpdateNode(
+    $input: UpdateNodeInput!
+    $condition: ModelNodeConditionInput
+  ) {
+    updateNode(input: $input, condition: $condition) {
+      id
+      title
+      videoResources
+      webResources
+      bookResources
+      authorID
+      author {
+        id
+        fullname
+        email
+        username
+        bio
+        location
+        isAdmin
+        image
+        posts {
+          nextToken
+        }
+        nodesCreated {
+          nextToken
+        }
+        createdAt
+        updatedAt
+        owner
+      }
+      createdAt
+      updatedAt
+      owner
+    }
+  }
+`;
+export const deleteNode = /* GraphQL */ `
+  mutation DeleteNode(
+    $input: DeleteNodeInput!
+    $condition: ModelNodeConditionInput
+  ) {
+    deleteNode(input: $input, condition: $condition) {
+      id
+      title
+      videoResources
+      webResources
+      bookResources
+      authorID
+      author {
+        id
+        fullname
+        email
+        username
+        bio
+        location
+        isAdmin
+        image
+        posts {
+          nextToken
+        }
+        nodesCreated {
+          nextToken
+        }
+        createdAt
+        updatedAt
+        owner
+      }
+      createdAt
+      updatedAt
+      owner
+    }
+  }
+`;
+export const createPost = /* GraphQL */ `
+  mutation CreatePost(
+    $input: CreatePostInput!
+    $condition: ModelPostConditionInput
+  ) {
+    createPost(input: $input, condition: $condition) {
+      id
+      content
+      category {
+        id
+        title
+        description
+        createdAt
+        updatedAt
+        owner
+      }
+      authorID
+      author {
+        id
+        fullname
+        email
+        username
+        bio
+        location
+        isAdmin
+        image
+        posts {
+          nextToken
+        }
+        nodesCreated {
+          nextToken
+        }
+        createdAt
+        updatedAt
+        owner
+      }
+      createdAt
+      updatedAt
+      postCategoryId
+      owner
+    }
+  }
+`;
+export const updatePost = /* GraphQL */ `
+  mutation UpdatePost(
+    $input: UpdatePostInput!
+    $condition: ModelPostConditionInput
+  ) {
+    updatePost(input: $input, condition: $condition) {
+      id
+      content
+      category {
+        id
+        title
+        description
+        createdAt
+        updatedAt
+        owner
+      }
+      authorID
+      author {
+        id
+        fullname
+        email
+        username
+        bio
+        location
+        isAdmin
+        image
+        posts {
+          nextToken
+        }
+        nodesCreated {
+          nextToken
+        }
+        createdAt
+        updatedAt
+        owner
+      }
+      createdAt
+      updatedAt
+      postCategoryId
+      owner
+    }
+  }
+`;
+export const deletePost = /* GraphQL */ `
+  mutation DeletePost(
+    $input: DeletePostInput!
+    $condition: ModelPostConditionInput
+  ) {
+    deletePost(input: $input, condition: $condition) {
+      id
+      content
+      category {
+        id
+        title
+        description
+        createdAt
+        updatedAt
+        owner
+      }
+      authorID
+      author {
+        id
+        fullname
+        email
+        username
+        bio
+        location
+        isAdmin
+        image
+        posts {
+          nextToken
+        }
+        nodesCreated {
+          nextToken
+        }
+        createdAt
+        updatedAt
+        owner
+      }
+      createdAt
+      updatedAt
+      postCategoryId
+      owner
     }
   }
 `;
