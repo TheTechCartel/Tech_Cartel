c310e7f53a65b0a03fa081b3d8fde1ff15a9b2e6
Joseph Enoch <enochjoseph01@gmail.com>
2022-09-08 18:43:31 +0100

    feat: add the function to sign user up

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
 import SignIn from "./pages/SignIn";
+import awsmobile from "./aws-exports";
+import { Amplify } from "aws-amplify";
+
+
+Amplify.configure(awsmobile)
 
 function App() {
+
   return (
    <main className="font-clashGrotesk">
     <SignIn/>
--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React, {  useState } from 'react'
 import { Button, Input, Logo } from '../components/shared'
 import Select from '../components/shared/Select'
-
+import{ Auth} from "aws-amplify"
+import countries from "countries-list"
 const SignIn = () => {
-  const options = ["Abuja", "Lagos", "New York"]
+  const freshData = {
+    email:"",
+    name:"",
+    location:"",
+    password:"",
+    confirmPassword:""
+  }
+  const options = Object.values(countries?.countries)
+  const data = options.map(option=>option?.name)
+  const [inputData, setInputData] = useState({...freshData})
+
+  const handleChange = (e) =>{
+    const { name, value} = e.target
+    setInputData(prevState=>{
+      const newArr = {...prevState, [name]: value}
+      return {...newArr}
+    })
+  }
+  const  handleSignIn = async (e)=>{
+    e.preventDefault()
+    if(inputData?.password !== inputData?.confirmPassword) return false
+    try {
+      const { user } = await Auth.signUp({
+          username: inputData?.email,
+          password: inputData?.password,
+          attributes: {
+              email:inputData?.email,
+              address: inputData?.location
+          } 
+         
+      });
+      console.log(user);
+  } catch (error) {
+      console.log('error signing up:', error);
+  }
+
+  }
   return (
     <>
       <title> Tech Cartel ~ Sign Up</title>
@@ -12,39 +49,62 @@ const SignIn = () => {
       <section className="w-screen h-screen bg-tcBG flex justify-center items-center flex-col">
 
         <span className="hidden lg:inline mb-10">
-          <Logo showLarge/>
+          <Logo/>
         </span>
-        <div 
-          className="w-3/4 max-w-[620px] h-screen lg:h-[700px] min-w-[500px] justify-evenly bg-white  rounded-[10px] flex flex-col items-center">
-            <span><Logo/></span>
+        <form 
+          className="w-3/4 max-w-[620px] h-screen lg:h-[700px] min-w-[500px] justify-evenly bg-white  rounded-[10px] flex flex-col items-center"
+          onSubmit={handleSignIn}
+        >
+            <span className="lg:hidden">
+              <Logo/>
+            </span>
             <Input 
               label="Full Name"
               placeholder="Enter your First and Last name"
               width="w-[70%]"
+              value={inputData?.name}
+              onChange={handleChange}
+              name="name"
+              required
             />
             <Input 
               label="Email"
               placeholder="example@techcartel.com"
               width="w-[70%]"
+              value={inputData?.email}
+              onChange={handleChange}
+              name="email"
+              required
             />
             <Select
               width="w-[70%]"
               defaultOption="Enter Location"  
-              options={options}
+              options={data}
               label="Choose your Country"
+              value={inputData?.location}
+              onChange={handleChange}
+              name="location"
+              required
             />
             <Input 
               width="w-[70%]"
               placeholder="Enter your Password"
               type="password"
               label="Password"
+              value={inputData?.password}
+              onChange={handleChange}
+              name="password"
+              required
             />
-
             <Input 
               width="w-[70%]"
               placeholder="Confirm your Password"
               type="password"
               label="Confirm Password"
+              value={inputData?.confirmPassword}
+              onChange={handleChange}
+              name="confirmPassword"
+              required
             />
             <Button 
               primary
@@ -56,7 +116,7 @@ const SignIn = () => {
               Already have an account?
               <span className="text-primary underline"> Log in</span>
             </span>
-        </div>
+        </form>
 
       </section>
 
