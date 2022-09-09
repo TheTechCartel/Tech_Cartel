b02b81bb5c33b14f64dd7f6c963731eaa98cb611
Joseph Enoch <enochjoseph01@gmail.com>
2022-09-09 19:30:50 +0100

    ...fixes

--- src/components/shared/index.jsx
+++ src/components/shared/index.jsx
@@ -1,10 +1,11 @@
 import Input from "./Input";
 import Button from "./Button";
 import Logo from "./Logo";
-
+import Select from "./Select";
 
 export {
     Input,
     Button,
     Logo,
+    Select
 }
