e9d6868557564452112e07f5a499736ada793af6
Joseph Enoch <enochjoseph01@gmail.com>
2022-09-14 22:21:22 +0100

    add routes

--- src/routes/PublicRoutes.jsx
+++ src/routes/PublicRoutes.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Navigate } from 'react-router-dom'
 import { protectedRoutes } from '../utils/listOfRoutes'
-import { Router } from 'react-router-dom'
 const PublicRoutes = ({children}) => {
     
     return <Navigate to="/dashboard"/>
